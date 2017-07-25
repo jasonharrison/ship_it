56a65350141ae883a8eb41d5eb33f255148ab7d5
Jason Harrison <me@jasonharrison.us>
2017-07-25 18:46:59 -0400

    views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :drop_off_packages
+  resources :drop_off_packages do
+    collection do
+      get 'receipt'
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
