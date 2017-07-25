955e833d9629c8bfd2f5444cc751474d9e7bc8a0
Jason Harrison <me@jasonharrison.us>
2017-07-25 18:17:05 -0400

    validate DropOffPackage is a valid tracking number

--- app/models/drop_off_package.rb
+++ app/models/drop_off_package.rb
@@ -1,4 +1,4 @@
 class DropOffPackage < ApplicationRecord
   belongs_to :user
-  validates :tracking_ref, presence: true
+  validates :tracking_ref, presence: true, tracking_number: { message: "is not valid" }
 end
